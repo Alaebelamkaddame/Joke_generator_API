7a9ea5b038928c378cd10502810d2c2ec1679f01
Alae <alaebelamkaddame@gmail.com>
2024-11-30 17:09:07 +0100

    Feature update

---
+++ src/component/Button.jsx
@@ -0,0 +1,10 @@
+
+
+const Button = (props) => {
+    return <button onClick={props.callApi}>
+        Click to generate a joke.
+    </button>;
+}
+
+// Export Button Component
+export default Button;
